78e7ddab1b739f9ffe73d55d9884a5310fdfba64
Jeff Knaggs <mail@jeef3.com>
2015-03-13 09:06:38 +1300

    Really basic time display

--- app/components/Main.react.js
+++ app/components/Main.react.js
@@ -1,11 +1,15 @@
 'use strict';
 
 import React from 'react';
+import moment from 'moment-timezone';
 
 import PeopleStore from '../stores/PeopleStore';
 
 function getState() {
+  var now = moment();
+
   return {
+    now: now,
     people: PeopleStore.all()
   };
 }
@@ -13,15 +17,25 @@ function getState() {
 class Main extends React.Component {
   constructor() {
     this.state = getState();
+
+    setInterval(() => {
+      this.setState(getState());
+    }, 1000);
   }
 
   render() {
+    var localTime = this.state.now.tz('Pacific/Auckland').format('LTS');
+
     return (
-      <ul>
-        {this.state.people.map((person, i) => {
-          return <div key={i}>{person.name}</div>;
-        })}
-      </ul>
+      <div>
+        <div>{localTime}</div>
+        <ul>
+          {this.state.people.map((person, i) => {
+            var displayTime = this.state.now.tz(person.tz).format('HH:mm:ss');
+            return <div key={i}>{person.name} - {displayTime}</div>;
+          })}
+        </ul>
+      </div>
     );
   }
 }
