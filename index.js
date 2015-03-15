a76f563c99e3e7c6d21810b841d0b2ec824edb82
Jeff Knaggs <mail@jeef3.com>
2015-03-15 22:32:57 +1300

    Load people from ENV

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('dotenv').load();
+
 var express = require('express');
 var exphbs  = require('express-handlebars');
 
@@ -12,12 +14,12 @@ app.set('port', (process.env.PORT || 9000));
 if ('development' === app.get('env')) {
   app.use(require('connect-livereload')({ port: 35729 }));
 }
-
+console.log(process.env.PEOPLE);
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.render('app', { 
-    people: JSON.stringify(require('./people.json')),
+    people: process.env.PEOPLE,
     layout: false
   });
 });
