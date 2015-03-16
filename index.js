20d9a7742e74d0c4b3953a83ad716fa3dc2153b8
Jeff Knaggs <mail@jeef3.com>
2015-03-16 21:09:53 +1300

    Didn't need to output that

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.set('port', (process.env.PORT || 9000));
 if ('development' === app.get('env')) {
   app.use(require('connect-livereload')({ port: 35729 }));
 }
-console.log(process.env.PEOPLE);
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
